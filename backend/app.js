e871916af90cbf475cc04b11c968516361e28e08
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2024-12-30 17:52:12 +0530

    Initialized Backend of the Chat Application.

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res)=> {
+    res.send('Good Morning!');
+});
+
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import http from 'http';
+import app from './app.js';
+
+const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+server.listen(port, ()=> {
+  console.log(`Server is Running on Port ${port}`);
+})
